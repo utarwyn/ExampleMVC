5de1c20bca1298ac028e20e247408c447f779c9d
Utarwyn <maxime.malgorn@laposte.net>
2019-09-21 11:29:29 +0200

    Add a config sample file

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+$config = array(
+	"databases" => array(
+		"dev" => array(
+			"db_name" => "blogmvc",
+			"db_user" => "root",
+			"db_pass" => "root",
+			"db_host" => "localhost"
+		)
+	),
+	"database" => "dev",
+
+	"template" => "default"
+);
